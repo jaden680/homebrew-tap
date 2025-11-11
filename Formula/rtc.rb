cbcaf05978902cc6740f652252c69245b19b942b
jaden.seo(제이든) <60868686+jaden680@users.noreply.github.com>
2025-11-11 12:59:07 +0900

    Update RTC formula to version 0.0.4

--- Formula/rtc.rb
+++ Formula/rtc.rb
@@ -1,7 +1,7 @@
 class Rtc < Formula
     desc "RT Ticket Check CLI" 
     homepage "https://github.com/jaden680/rt_check" 
-    url "https://github.com/jaden680/rt_check/archive/refs/tags/0.0.3.tar.gz"
+    url "https://github.com/jaden680/rt_check/archive/refs/tags/0.0.4.tar.gz"
     sha256 "b595c3335eb5442cede00556f3cad1ade50f27a96e56905189904cd3c33a36da"
     version "0.0.4"
     depends_on "poetry" 
